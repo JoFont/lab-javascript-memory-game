b96f477e2d53eeae9b4a7a1bc67806dd49c3ef11
Diogo Marques <diogo@fontainhas.com>
2019-10-29 16:50:30 +0000

    init

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,18 +1,38 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // Add the rest of the properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ...
+    let currentIndex = this.cards.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = this.cards[currentIndex];
+      this.cards[currentIndex] = this.cards[randomIndex];
+      this.cards[randomIndex] = temporaryValue;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ...
+    this.pairsClicked++
+    if(card1 === card2) {
+      this.pairsGuessed ++;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isFinished() {
-    // ...
+    return this.pairsGuessed === this.cards.length / 2 ? true : false;
   }
 }
